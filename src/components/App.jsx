76de12a9556c1fa788d245ace059ec421fb3608d
Taras-Melnyk1987 <dr.melnuk1987@gmail.com>
2022-02-23 04:48:22 +0200

    app work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,109 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import Form from './Form';
+import Contacts from './Contacts';
+import Section from './Section';
+import Filter from './Filter';
+import styled from 'styled-components';
+import { nanoid } from 'nanoid';
+
+const PhonebookApp = styled.div`
+  margin: 25px auto;
+  min-width: 400px;
+  max-width: 500px;
+  padding: 15px;
+  border-radius: 10px;
+  background-color: #fff;
+`;
+
+export default class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleFormContact = ({ name, number }) => {
+    const editedNumber = this.numberFormatting(number);
+
+    const contact = {
+      id: nanoid(),
+      name,
+      number: editedNumber,
+    };
+
+    this.setState(({ contacts }) => ({ contacts: [contact, ...contacts] }));
+  };
+
+  handleFilterReset = () => {
+    this.clearFilter();
+  };
+
+  numberFormatting = number => {
+    console.log(number);
+    const array = [...number];
+    for (let i = 3; i < array.length - 1; i += 3) {
+      array.splice(i, 0, '-');
+    }
+    console.log();
+    return array.join('');
+  };
+
+  checkName = name => {
+    const { contacts } = this.state;
+    const normalizeName = name.toLowerCase();
+    const nameToAdd = contacts.find(
+      contact => contact.name.toLowerCase() === normalizeName,
+    );
+
+    return nameToAdd;
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  deleteContact = index => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== index),
+    }));
+  };
+
+  clearFilter = () => {
+    this.setState({
+      filter: '',
+    });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+
+    const normalizedFilter = filter.toLocaleLowerCase();
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+
+    return (
+      <PhonebookApp>
+        <Section title={'Phonebook'}>
+          <Form getSubmitData={this.handleFormContact} />
+        </Section>
+        <Section title={'Contacts'}>
+          <Filter
+            value={filter}
+            onChange={this.changeFilter}
+            onClick={this.handleFilterReset}
+          />
+          <Contacts
+            list={filteredContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        </Section>
+      </PhonebookApp>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from 'components/App';
-import './index.styled';
+import App from './components/App';
+import GlobalStyle from './baseStyles/GlobalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
